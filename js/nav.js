e8ed7db39753dca00b6a9e9c543df7b99184fd7c
G-smart <smartmirror.ca@gmail.com>
2018-11-28 00:05:38 -0800

    update schedule and lessons page
    add schedule.html , updated js and menu nav

--- js/nav.js
+++ js/nav.js
@@ -5,7 +5,7 @@
 	  nav: document.querySelector('nav'),
   
 	  doToggle: function(e) {
-		e.preventDefault();
+		
 		this.navToggle.classList.toggle('expanded');
 		this.nav.classList.toggle('expanded');
 	  }
@@ -15,3 +15,18 @@
 	hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
   
   }());
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
